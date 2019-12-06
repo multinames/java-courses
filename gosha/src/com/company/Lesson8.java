2daa550b1168d5e59b7538906d5edbb4e1f1e92c
skuridin@gmail.com <Ghbdtn00>
2019-12-06 17:19:03 +0300

    Lesson 8

---
+++ gosha/src/com/company/Lesson8.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import sun.font.TrueTypeFont;
+
+import java.util.Scanner;
+
+public class Lesson8 {
+
+    public static void main (String[] args) {
+        for (int i = 10; i <= 20; i++)
+            System.out.println(i*i);
+
+
+        /*boolean isTrue = true;
+        int j = 0;
+        while ( j < 100) {
+            j +=1;
+            if (j % 2 == 0)
+                continue;
+            System.out.println(j);
+
+        }
+*/
+/*
+        int x = 13;
+        do {
+            x--;
+            System.out.println(x);
+        }   while (x > 10);
+*/
+
+    }
+}
