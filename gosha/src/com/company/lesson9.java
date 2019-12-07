4abe96508161618155407dfd9938ed9b0dad2ef7
skuridin@gmail.com <Ghbdtn00>
2019-12-07 15:08:03 +0300

    Lesson 9 array

---
+++ gosha/src/com/company/lesson9.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner; // подключили класс Scanner
+
+public class lesson9 {
+    public static void main (String[] args) {
+        int[] a;
+        int n;
+        Scanner in = new Scanner(System.in); // Создаем объект in класса Scanner
+        System.out.print("Enter number of array: ");
+        n = in.nextInt();
+        a = new int[n];              //выделение памяти под массив
+        for (int i = 0; i < n ; i++ ) {
+            System.out.print("Enter a[" + i +"] = ");
+            a[i] = in.nextInt ();
+        }
+    }
+}
