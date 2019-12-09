99f8d9d9aef35916a955f1e771855a8fbdff9e09
skuridin@gmail.com <Ghbdtn00>
2019-12-09 23:06:07 +0300

    Life class...

---
+++ Life/src/com/company/Human.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Human {
+
+    public String sex;
+    public String name;
+    public String period;
+
+
+    //конструктор
+    public Human (String s, String n, String p) {
+        sex = s;
+        name = n;
+        period = p;
+    }
+
+}
