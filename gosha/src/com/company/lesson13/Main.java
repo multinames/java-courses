170d0f69e8c29bb5752a7f509f9f54f0fabaeaf9
skuridin@gmail.com <Ghbdtn00>
2019-12-09 14:42:46 +0300

    Lesson 13 add new var

--- gosha/src/com/company/lesson13/Main.java
+++ gosha/src/com/company/lesson13/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Person ser = new Person(); // ser - объект
         ser.height = 166;
+        ser.say("Geogre");
         System.out.println(ser.height); // вызывали поле height из класса Person для объекта ser
         Person vlad = new Person();
         System.out.print(vlad.height);
--- gosha/src/com/company/lesson13/Person.java
+++ gosha/src/com/company/lesson13/Person.java
@@ -2,4 +2,9 @@ package com.company.lesson13;
 
 public class Person {
     public int height = 180; // public (досутпен) - модификатор доступа
+
+    public void say (String name) {
+        System.out.println("Hello, " + name);
+    }
+
 }
