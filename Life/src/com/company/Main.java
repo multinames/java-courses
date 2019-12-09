db00ff8dae0742732478237d6bb5f7a65c326af7
skuridin@gmail.com <Ghbdtn00>
2019-12-09 23:07:08 +0300

    add Main Life class

---
+++ Life/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Human ser = new Human("man","Sergey", "youth");
+        System.out.println(ser.sex + " " + ser.name + " " + ser.period);
+    }
+}
