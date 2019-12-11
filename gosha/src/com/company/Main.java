98bfea9dbf8b3d7b988119372e0c6ab980b64723
skuridin@gmail.com <Ghbdtn00>
2019-12-11 14:05:25 +0300

    first Main

---
+++ gosha/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Введите число с точкой: ");
+        Scanner num1 = new Scanner(System.in);
+        System.out.println("Ваше число -" + num1.nextFloat ());
+
+    }
+}
