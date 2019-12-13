142cfdddae7c09cf0c0ac6e312dd02278c636e6b
skuridin@gmail.com <Ghbdtn00>
2019-12-13 15:01:03 +0300

    Add cheange Class in lesson 19

---
+++ gosha/src/com/company/lesson19/Main.java
@@ -0,0 +1,9 @@
+package com.company.lesson19;
+// Пакеты
+// Область видимомсти
+public class Main {
+    static int x1 = 0; // переменная видна в др. методах;
+    public static void main(String[] args) {
+            System.out.print(x1);
+    }
+    }
