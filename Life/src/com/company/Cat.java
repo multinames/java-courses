566831a49680437e175cb173f38d482abb3d74c8
skuridin@gmail.com <Ghbdtn00>
2019-12-16 00:42:19 +0300

    Cats Class and Main

---
+++ Life/src/com/company/Cat.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Cat {
+    String name;
+    int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
---
+++ Life/src/com/company/MainCat.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MainCat {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Маркиз", 15);
+        System.out.println(cat1.name + " " + cat1.age);
+
+
+    }
+
+}
