4d7ab76e75373be2d0142439f5a00b3d6737db00
skuridin@gmail.com <Ghbdtn00>
2019-12-16 18:53:11 +0300

    Duck class interface

---
+++ Life/src/com/company/Duck.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Duck implements Swimmable {
+
+    public void swim() { //То же метод swim
+        System.out.println("Уточка, плыви!");
+    }
+
+    public static void main(String[] args) {
+
+        Duck duck = new Duck();
+        duck.swim();
+    }
+}
---
+++ Life/src/com/company/Swimmable.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface Swimmable	{
+
+    public void swim(); // Метод swim
+}
