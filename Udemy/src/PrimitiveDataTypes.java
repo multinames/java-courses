810f7267706a65ab79895b78f174cbd298471468
skuridin@gmail.com <Ghbdtn00>
2019-12-18 19:15:12 +0300

    Example Udemy course for Java

---
+++ Udemy/src/PrimitiveDataTypes.java
@@ -0,0 +1,30 @@
+    public class PrimitiveDataTypes {
+        public static void main(String[] Agrs) {
+            byte b1 = 10;
+
+            short s1 = 5;
+            short s3 = 0;
+
+            int i2 = 500;
+
+            long l1 = 1000;
+            long l2 = 10000000000L;
+
+            float f1 = 3.14F;
+            float f2 = 20;
+
+            double d1 = 5.5;
+
+                       char c1 = 'A';
+            char c2 = 120;
+            char c3 = '\u0105';
+
+
+            System.out.println("Char "+ " " + c1 + " " + c2 + " " + c3);
+
+
+        }
+
+    }
+
+
