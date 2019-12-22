952bae6983adc51984316483b8a19bd59ccaf6db
skuridin@gmail.com <Ghbdtn00>
2019-12-23 00:17:38 +0300

    cheange code for lesson 5

--- Udemy/src/com/company/Lesson5/Test20.java
+++ Udemy/src/com/company/Lesson5/Test20.java
@@ -8,18 +8,22 @@ public class Test20 {  // Шаблон метода. Описываем логи
         return result; // вывод результата; последняя строка метода
     }
 
-    int sredneeArifm(int a1, int b1, int c1) {  // вызов метода в методе
-        int result2 = summa(a1, b1, c1)/3 ;
+    int sredneeArifm(int a1, int b1, int c1) {
+        int result2 = summa(a1, b1, c1)/3 ;  // вызов метода в методе
         return result2;
     }
 }
 
 class  Test21{ // реализация метода в новом классе
     public static void main(String[] args) {
+
         Test20 t = new Test20();
         int Summa3Chisel = t.summa(1,2,3);
+
+
         System.out.println(Summa3Chisel);
         System.out.println(t.summa(10,20,30));
+
         System.out.println(t.sredneeArifm(20,40,60));
     }
 
