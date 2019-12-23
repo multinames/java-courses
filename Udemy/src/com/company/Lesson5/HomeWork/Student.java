c5db37af91d6aba177c0af01f19118d9f6308529
skuridin@gmail.com <Ghbdtn00>
2019-12-23 14:35:28 +0300

    HomeWork for lesson 5 Student

---
+++ Udemy/src/com/company/Lesson5/HomeWork/Student.java
@@ -0,0 +1,63 @@
+package com.company.Lesson5.HomeWork;
+
+import java.util.PrimitiveIterator;
+
+public class Student {
+    int studient_card;
+    String student_name;
+    String student_lastname;
+    int course;
+    int mathAverageGrade;
+    int ecomAverageGrade;
+    double langAverageGrade;
+
+    void AverageGrade (String lastname, int math, int econ, int lang){
+        double srednyayaOcenka = (math+econ+lang) /3;
+        System.out.println("Средня оценка студента " + lastname + " : " + srednyayaOcenka);
+    }
+}
+
+class StudentTest {
+
+    public static void main(String[] args) {
+        Student Piter = new Student();
+        Student Mike = new Student();
+        Student Jonh = new Student();
+
+        Piter.studient_card = 110;
+        Piter.student_name = "Петя";
+        Piter.student_lastname = "Иванов";
+        Piter.course = 2;
+        Piter.mathAverageGrade = 4;
+        Piter.ecomAverageGrade = 3;
+        Piter.langAverageGrade = 5;
+
+        Piter.AverageGrade(Piter.student_lastname,4,3,5);
+
+        Mike.studient_card = 115;
+        Mike.student_name = "Миша";
+        Mike.student_lastname = "Петров";
+        Mike.course = 1;
+        Mike.mathAverageGrade = 5;
+        Mike.ecomAverageGrade = 4;
+        Mike.langAverageGrade = 4;
+
+        Piter.AverageGrade(Mike.student_lastname,5,4,4);
+
+        Jonh.studient_card = 120;
+        Jonh.student_name = "Женя";
+        Jonh.student_lastname = "Сидиров";
+        Jonh.course = 3;
+        Jonh.mathAverageGrade = 4;
+        Jonh.ecomAverageGrade = 3;
+        Jonh.langAverageGrade = 4;
+
+        Piter.AverageGrade(Jonh.student_lastname, 4,3,4);
+
+        /*System.out.println("Средняя оценка студента " + Piter.student_name + " " + Piter.student_lastname + " " + (Piter.mathAverageGrade + Piter.ecomAverageGrade + Piter.langAverageGrade) / 3);
+        System.out.println("Средняя оценка студента " + Mike.student_name + " " + Mike.student_lastname + " " + (Mike.mathAverageGrade + Mike.ecomAverageGrade + Mike.langAverageGrade) / 3);
+        System.out.println("Средняя оценка студента " + Jonh.student_name + " " + Jonh.student_lastname + " " + (Jonh.mathAverageGrade + Jonh.ecomAverageGrade + Jonh.langAverageGrade) / 3);*/
+
+    }
+
+}
