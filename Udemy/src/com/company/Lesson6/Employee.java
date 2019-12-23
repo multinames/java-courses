7d3ec8df392aab648f496165e89a8371097db26b
skuridin@gmail.com <Ghbdtn00>
2019-12-23 19:01:30 +0300

    Lesson 5 Employee new code

--- Udemy/src/com/company/Lesson6/Employee.java
+++ Udemy/src/com/company/Lesson6/Employee.java
@@ -4,19 +4,17 @@ public class Employee {
     // Определяем параметры работника в конструкторе
 
     Employee (int id2, String  surname2, int age2){
-        this(surname2, age2);
-        id = id2;
-
+        this (id2,surname2, age2,0.0,null);
     }
 
     Employee (String surname3, int age3) {
-        surname = surname3;
-        age = age3;
+        this (0,surname3, age3,0.0,null);
     }
 
     Employee (int id4, String  surname4, int age4, double salary4, String department4){
-        this(id4,surname4, age4);
         id = id4;
+        surname = surname4;
+        age = age4;
         salary = salary4;
         department = department4;
     }
