d8dbbdd319dca2c0c9cc9010d8b7ccc9f1146045
skuridin@gmail.com <Ghbdtn00>
2019-12-27 22:39:51 +0300

    Lesson 8 HomeWork fix

--- Udemy/src/com/company/Lesson8/HomeWork/Home2.java
+++ Udemy/src/com/company/Lesson8/HomeWork/Home2.java
@@ -1,24 +1,22 @@
 package com.company.Lesson8.HomeWork;
 
 public class Home2 {
-    static final double pi = 3.14;
+    public static final double pi = 3.14;
 
-    void ploshadKruga(double radius){
-        double s;
-        s = pi*radius*radius;
-        System.out.println(s);
+    public double ploshad(double radius){
+        double s = pi*radius*radius;
+        return s;
     }
 
-    static void dlinnaOkruzhnosti(double radius){
-        double s;
-        s = 2*pi*radius;
-        System.out.println(s);
+    public static double dlinna(double radius){
+        double s = 2*pi*radius;
+        return s;
     }
 
-    void ShowInfo(double radius){
-        System.out.println(radius);
-        System.out.println(pi*radius*radius);
-        System.out.println(2*pi*radius);
+    void ShowInfo(double radius2){
+        System.out.println("Радиус: " +radius2);
+        System.out.println("Площадь круга: " + ploshad(radius2));
+        System.out.println("Длинна окружности: " + dlinna(radius2));
             }
 
 }
@@ -27,9 +25,9 @@ class Home2Test{
     public static void main(String[] args) {
 
         Home2 r1 = new Home2();
-        r1.ploshadKruga(12);
-        Home2.dlinnaOkruzhnosti(14);
-        r1.ShowInfo(14);
+        r1.ploshad(5);
+        Home2.dlinna(7.5);
+        r1.ShowInfo(3);
 
     }
 
