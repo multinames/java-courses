a273a770c506ec8d27c8d4a101d8177ebeb28ac6
skuridin@gmail.com <Ghbdtn00>
2019-12-29 21:21:32 +0300

    Lesson 9 change

---
+++ Udemy/src/com/company/Lesson9/Student.java
@@ -0,0 +1,14 @@
+package com.company.Lesson9;
+
+public class Student {
+
+}
+
+class StudentTest{
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3;
+        st1 = null; // подготовка объекта к удалению
+    }
+}
