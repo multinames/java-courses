9c3bc4679dc09b426795a6677ea9c3d148627f27
skuridin@gmail.com <Ghbdtn00>
2019-12-29 23:21:18 +0300

    Lesson 9 HomeWork

---
+++ Udemy/src/com/company/Lesson9/HomeWork/Home1.java
@@ -0,0 +1,16 @@
+package com.company.Lesson9.HomeWork;
+
+public class Home1 {
+
+    public static void abc(){
+        String s1 = new String("ABC");
+        String s2 = new String("DEF");
+    }
+
+    public static void main(String[] args) {
+        Home1 h1 = new Home1();
+        abc();
+        abc();
+        String s1 = new String("ABC");
+    }
+}
