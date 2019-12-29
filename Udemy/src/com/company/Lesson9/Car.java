82b8844a3d361707dee0ca0ad7d03bac9dee6ebd
skuridin@gmail.com <Ghbdtn00>
2019-12-29 19:17:34 +0300

    Lesson 9

---
+++ Udemy/src/com/company/Lesson9/Car.java
@@ -0,0 +1,37 @@
+package com.company.Lesson9;
+
+public class Car {
+
+    int s = 10;
+    String color;
+    String engine;
+    static int count;
+
+    Car(String color, String engine ){  // Конструктор
+        int x=5;
+        int y = x+10;
+        count++;
+        this.color = color;   //this.color - означает принадлежность к текущему обьекту ;= color -переменная из параметра.
+        this.engine = engine;
+    }
+
+
+/*    public  void showColor(){
+        System.out.println("Цвет машины : " + color);
+        changeColor("Red");
+    }*/
+
+    public void changeColor(String color){
+        System.out.println(color);
+
+    }
+
+    public static void main(String[] args) {
+        Car c = new Car ("red", "V6");
+        System.out.println(c.color);
+        c.changeColor("black");
+        System.out.println(c.color);
+
+
+    }
+}
