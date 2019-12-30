92236c221c6a53b0e53aaf5417bb6c7d99ecd0f1
skuridin@gmail.com <Ghbdtn00>
2019-12-30 19:48:16 +0300

    Lesson 10 fix

---
+++ Udemy/src/com/company/Lesson10/A.java
@@ -0,0 +1,18 @@
+package com.company.Lesson10;
+
+// import com.company.Lesson4.*;
+import static com.company.Lesson9.Car.a;
+import static com.company.Lesson9.Car.count;
+
+
+public class A {
+
+    static int count = 99;
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(count);
+
+    }
+
+}
+
