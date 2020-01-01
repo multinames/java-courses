d88a4ff8c0bda5c46655080f8f9327f7c47e73cc
skuridin@gmail.com <Ghbdtn00>
2020-01-01 17:01:38 +0300

    Lesson 12

---
+++ Udemy/src/Lesson12/Test10.java
@@ -0,0 +1,15 @@
+package Lesson12;
+
+public class Test10 {
+    public static void main(String[] args) {
+        int a = 30;
+
+        if (a < 20) {
+            System.out.println("a < 20");
+
+            a++;
+        }
+
+    }
+
+}
