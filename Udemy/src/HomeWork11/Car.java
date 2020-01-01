780bea5e6ce8d101dfd3e76d9917272bfc35e5a4
skuridin@gmail.com <Ghbdtn00>
2020-01-01 16:46:59 +0300

    HomeWork 11 fix

--- Udemy/src/HomeWork11/Car.java
+++ Udemy/src/HomeWork11/Car.java
@@ -5,7 +5,7 @@ public class Car {
     String engine;
     int doors;
 
-    Car (String color, String engine, int doors) {
+    Car (String color, String engine, int doors) { // Конструктор
         this.color = color;
         this.engine = engine;
         this.doors = doors;
@@ -15,11 +15,11 @@ public class Car {
 
 class CarTest{
 
-    public static void changeDoors(Car c, int d){
+    public static void changeDoors(Car c, int d){  // метод который менят кол-во дверй
     c.doors = d;
     }
 
-    public static void changeColor (Car c1, Car c2) {
+    public static void changeColor (Car c1, Car c2) { // метод смены цветов
         String c3 = c1.color;
         c1.color = c2.color;
         c2.color = c3;
@@ -29,15 +29,11 @@ class CarTest{
         Car car1 = new Car("red", "V6", 3);
         Car car2 = new Car("black", "V8", 5);
 
-
-        System.out.println(car1.color);
-        changeDoors(car1,4);
-        changeColor(car1, car2);
+        changeDoors(car1,4); // изменение количества дверей
+        changeColor(car1, car2); //смена местами цвета двух машин
 
         System.out.println("Car 1: цвет " +car1.color + ", двигатель " +car1.engine +", кол-во дверей "+ car1.doors);
         System.out.println("Car 2: цвет " +car2.color + ", двигатель " +car2.engine +", кол-во дверей "+ car2.doors);
 
     }
 }
-
-
