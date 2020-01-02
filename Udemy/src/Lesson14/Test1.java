78b727db74dd212809c638088ae1df0ee3d5a563
skuridin@gmail.com <Ghbdtn00>
2020-01-02 21:18:49 +0300

    Lesson 14 for

--- Udemy/src/Lesson14/Test1.java
+++ Udemy/src/Lesson14/Test1.java
@@ -1,9 +1,9 @@
 package Lesson14;
-// for....
+// for loop
 
 public class Test1 {
     public static void main(String[] args) {
-        for (int i = 1; i <=10; i++) {
+        for (int i = 1; i <=10; i++) {  // условие boolean
             System.out.println("Урок номер 14");
         }
     }
