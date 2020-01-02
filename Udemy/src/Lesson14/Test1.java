19f57a89afbebc24ef11dbd6fd4866bbadc7c75a
skuridin@gmail.com <Ghbdtn00>
2020-01-02 21:14:12 +0300

    Lesson 14 for

---
+++ Udemy/src/Lesson14/Test1.java
@@ -0,0 +1,10 @@
+package Lesson14;
+// for....
+
+public class Test1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10; i++) {
+            System.out.println("Урок номер 14");
+        }
+    }
+}
