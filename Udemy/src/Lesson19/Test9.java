9b388d68fdcae15ca0ad22cd1c50ba602d06f63c
skuridin@gmail.com <Ghbdtn00>
2020-01-06 23:28:45 +0300

    Lesson 19 fix

--- Udemy/src/Lesson19/Test9.java
+++ Udemy/src/Lesson19/Test9.java
@@ -4,7 +4,7 @@ public class Test9 {
     public static void main(String[] args) {
         int [] array1 = {3,1,-2,0};
         int [] array2 = {-9, 1,5,2};
-        for (int i=0; i<array1.length && i < array2.length;++){
+        for (int i=0; i<array1.length && i < array2.length;i++){
             array1[i] =12;
             array2[i] = 3;
 
