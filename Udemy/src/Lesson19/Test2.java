09e38df5ff4f3a3459cd71b797ba6af959e47414
skuridin@gmail.com <Ghbdtn00>
2020-01-06 23:03:55 +0300

    Lesson 18 varargs

---
+++ Udemy/src/Lesson19/Test2.java
@@ -0,0 +1,16 @@
+package Lesson19;
+// varargs
+public class Test2 {
+    static void summa(String s, int ...a) {
+        int summa =0;
+        for (int i=0;i <a.length;i++){
+            summa+= a[i];
+        }
+        System.out.println(summa);
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        summa("ok", new int[]{1,2,3,4});
+    }
+}
