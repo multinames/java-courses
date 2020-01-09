cf363331929cc534b52f17bf8e6e119fa487cc90
skuridin@gmail.com <Ghbdtn00>
2020-01-09 19:38:18 +0300

    Lesson 19 HomeWork

--- Udemy/src/HomeWork19/HomeWork19.java
+++ Udemy/src/HomeWork19/HomeWork19.java
@@ -1,17 +1,27 @@
 package HomeWork19;
 
 public class HomeWork19 {
-    static void abc(String[] s, String ...n){
+    static void abc(String[] ...n){
+
+        String[] s = new String[]{"qwe","rty","asd","dcv"};
 
         for (int i=0; i <s.length;i++) {
-            for (int j = 0; j < n.length; j++)
-                if (s[j].equals(n[i])) {
-                    System.out.println("match: " + n[i]+" и " +s[j]);
-                }
+            for (int j = 0; j < n.length; j++) {
+                System.out.println("s[] "+s[i] + " n[]" + n);
+                /*if (s[i].equals(n[j])) {
+                    System.out.println("match: " + n[j] + " and " + s[i]);
+                    n[i] = null;
+                }*/
+            }
         }
+
+        /*for (int k=0; k <s.length; k++) {
+            System.out.println(s[k]);
+        }*/
     }
     public static void main(String[] args) {
-        abc(new String[]{"qaz","qwe","asd","vbn"}, new String[]{"qwe","rty","asd","dcv"});
+        // abc(args);
+        abc(new String[]{"dfg","fgh","asd","zxc"});
     }
 }
 
---
+++ Udemy/src/HomeWork19/Test1.java
@@ -0,0 +1,19 @@
+// package HomeWork19;
+
+public class Test1 {
+
+    public static void abc(String[] s, String ...n) {
+        //String[] s1 = new String[]{"qwe", "iop","zxc","werty","asdf"};
+        // String[] ar2 = new String[]{"qwe", "zxc","hjk","vght","asdf"};
+        for (int i=0; i< s.length; i++) {
+            for (int j=0; j< n.length; j++) {
+            if (s[i] == n[j]) {
+                System.out.println(s[i] + " " + n[j]);
+            }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        abc(args, new String[]{"qwe", "asd"});
+    }
+}
