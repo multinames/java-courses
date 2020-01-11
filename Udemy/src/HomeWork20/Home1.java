e92afdbcf725be75c1b36c1020cf16bddb4169b4
skuridin@gmail.com <Ghbdtn00>
2020-01-11 22:26:54 +0300

    HomeWork 20

--- Udemy/src/HomeWork20/Home1.java
+++ Udemy/src/HomeWork20/Home1.java
@@ -1,21 +1,25 @@
 package HomeWork20;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Home1 {
     public ArrayList<String> abc (String ...n){
         ArrayList<String> list = new ArrayList();
         for (String array:n) {
+            if (!list.contains(array)) {
 
-            list.add(new String(array));
+                list.add(array);
+            }
         }
+        Collections.sort(list);
         System.out.println(list);
         return list;
 
     }
-
     public static void main(String[] args) {
         Home1 hm1 = new Home1();
-        hm1.abc("qwey","asdf", "zxcv","ok","vtf");
+        hm1.abc("qwey","asdf", "zxcv","ok","vtf","asdf","ok");
+
     }
 }
