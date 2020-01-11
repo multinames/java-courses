175b3ac6775367b556d9b1affc92737fa16fa4ac
skuridin@gmail.com <Ghbdtn00>
2020-01-11 22:48:06 +0300

    HomeWork 21 Ternary operator

---
+++ Udemy/src/Lesson21/Test1.java
@@ -0,0 +1,9 @@
+package Lesson21;
+// Ternary operator
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 4;
+        System.out.println((a<4?7:"Privet"));
+
+    }
+}
