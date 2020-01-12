6d3ca3e0cb037f588e65d7fa4ae6c6f3f1bef940
skuridin@gmail.com <Ghbdtn00>
2020-01-12 16:50:11 +0300

    Lesson 22 Overloaded & Super constructors +

--- Udemy/src/Lesson22/Test4.java
+++ Udemy/src/Lesson22/Test4.java
@@ -5,9 +5,9 @@ public class Test4 {
 
 class Human2 {
     Human2 (String n) {
-        this(n, null); // Вызов Overloaded конструктора
+        this(n, null); // Вызов Overloaded конструктора Human2 (String n, String s)
     }
-    Human2 (String n, String s) {
+    Human2 (String n, String s) { // Вызов конструктора Object
         name = n;
         surename = s;
     }
@@ -17,11 +17,17 @@ String surename;
 }
 
 class Student2 extends Human2 {
-    Student2(){
-        super("Питер"); // вызов конструтктора супер-класса;
+
+    Student2() { // вызов конструктора Student2(int i)
+        this(5);
+        System.out.println("Hello!");
+    }
+
+    Student2(int i){
+        super("Питер"); // вызов конструтктора супер-класса Human2 (String n)
     };
     public static void main(String[] args) {
-        Student2 st = new Student2();
+        Student2 st = new Student2(); // вызов конструктора Student2()
     }
 }
 
