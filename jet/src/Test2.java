3de52f7faa2d53dd70c8a7f5f1e90ede8ad58335
cepera <skuridin@gmail.com>
2020-01-25 11:22:30 +0300

    Jet Lesson1

--- jet/src/Test2.java
+++ jet/src/Test2.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Введите число:");
@@ -8,11 +9,23 @@ public class Test2 {
 
         int result = Calculator.twoDigit(value);
         System.out.println(result);
+
+        int result2 = Calculator.threeDigitSum(value);
+        System.out.println(result2);
     }
 }
 
 class Calculator{
     public static  int twoDigit (int value) {
+
         return value %100;
     }
+
+    public static int threeDigitSum(int value){
+        int first = value / 100;
+        int second = value % 100 /10;
+        int third = value %10;
+
+        return first + second+ third;
     }
+}
---
+++ jet/src/Test3.java
@@ -0,0 +1,8 @@
+public class Test3 {
+// ручное приведение
+    public static void main(String[] args) {
+        byte a=10,b=20;
+        byte c;
+        c= (byte) (a+b);
+    }
+}
