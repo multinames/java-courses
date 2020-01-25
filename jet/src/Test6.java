4d61de8b02701263ef9c124ae67f1b5a976bc897
cepera <skuridin@gmail.com>
2020-01-25 12:35:33 +0300

    Jet Lesson1 Circle

---
+++ jet/src/Test6.java
@@ -0,0 +1,19 @@
+public class Test6 {
+    public static void main(String[] args) {
+        Iterable.printValues();
+    }
+}
+
+class Iterable {
+    public static void printValues(){
+/*        for (int i=9; i>=0; i--) {
+            System.out.println(i);*/
+
+    int i =10;
+    while (i<100){
+        System.out.println(i);
+            i++;
+
+        }
+    }
+}
