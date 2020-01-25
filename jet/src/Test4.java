0f617cf37737cb0826589dc88052c9c33b99866b
cepera <skuridin@gmail.com>
2020-01-25 12:35:44 +0300

    Jet Lesson1

--- jet/src/Test4.java
+++ jet/src/Test4.java
@@ -9,16 +9,15 @@ public class Test4 {
 class Calculator1 {
     public  static int maxInTreeValue (int a, int b, int c) {
 
-     /*   if (a > b) {
+        if (a > b) {
             if (a > c) {
                 return a;
             }
             if (b > c) {
                 return b;
             }
-            else return c;
+            return c;
         }
-        else return c;*/
-     return c;
+        return c;
     }
 }
