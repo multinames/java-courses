7d248fded1b948b339d7ede2414b648eef7b1db9
cepera <skuridin@gmail.com>
2020-01-25 19:57:21 +0300

    HomeWork 1 fix class name

--- jet/src/HomeWork1/Home1.java
+++ jet/src/HomeWork1/Home1.java
@@ -2,14 +2,14 @@ package HomeWork1;
 
 // Примитивные типы
 
-public class hw1 {
+public class Home1 {
     public static void main(String[] args) {
-        int a = 69, b = 23456, c=890 ;
+        int a = 69, b = 23456, c=891 ;
 
         System.out.println("Последняя цифра в "+a + " = "+a%10);
         System.out.println("Число десятков в "+a + " = "+a/10);
         System.out.println("Число десятков многозначного числа "+b + " = "+(b/10%10));
-        System.out.println("Сумма цифр трехзначного числа = "+ ((c/100)+(c/10%10)+(c%10)));
+        System.out.println("Сумма цифр трехзначного числа " + c + " = "+ ((c/100)+(c/10%10)+(c%10)));
     }
 }
 
