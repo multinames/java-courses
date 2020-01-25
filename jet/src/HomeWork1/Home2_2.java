0576538fe02e3188cfa47be8a7f5e476ebd9f5dc
cepera <skuridin@gmail.com>
2020-01-25 22:43:32 +0300

    HomeWork 2 if else part 3 fixde class

--- jet/src/HomeWork1/Home2_2.java
+++ jet/src/HomeWork1/Home2_2.java
@@ -7,20 +7,42 @@ public class Home2_2 {
         TwoNumbers t1 = new TwoNumbers();
         t1.abc(3,3);
         t1.abc(2,3);
+        System.out.println("");
+        t1.abc2(3,2);
+        t1.abc2(3,3);
     }
 }
 
 class TwoNumbers {
-
     public void abc(int a, int b){
 
         if (a!=b) {
+            System.out.print("Дано a="+a+ " и b=" +b);
             int c=a+b;
             a=c; b=c;
-            System.out.println("Значения не равны, новые значения "+a+" "+b);
+            System.out.println(", значения не равны, новые значения a="+a+" b="+b);
         }
-        else { a=0;b=0;
-            System.out.println("Значения равны, новые значения "+a+" "+b);
+        else {
+            System.out.print("Дано a="+a+ " и b=" +b);
+            a=0;b=0;
+            System.out.println(", значения равны, новые значения a="+a+" b="+b);
         }
+    }
+
+    public void abc2(int a, int b) {
+        if (a!=b) {
+            if(a >b) {
+                System.out.print("Дано a="+a+ " и b=" +b);
+                b=a;
+                System.out.println(", значения не равны, новые значения a="+a+" b="+b);
+
+            }
+        }
+        else {
+            System.out.print("Дано a="+a+ " и b=" +b);
+            a=0;b=0;
+            System.out.println(", значения равны, новые значения a="+a+" b="+b);
+        }
+
     }
 }
