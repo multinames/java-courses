91b2a69ca36438b0d73b97fa5aa5342ee834d276
cepera <skuridin@gmail.com>
2020-02-03 01:17:56 +0300

    Task Flower.java

--- Life/src/com/company/Lesson8/Flower.java
+++ Life/src/com/company/Lesson8/Flower.java
@@ -3,10 +3,30 @@ package com.company.Lesson8;
 public class Flower {
     String name;
     double price;
+
+    public void makeBouquet(int flower, double price, String name){
+
+        String[] arr = new String[flower];
+        for (int i=0; i<flower;i++) {
+            arr[i] = name;
+        }
+        System.out.print("Состав букета - (");
+        for (String i: arr
+             ) {
+            System.out.print(i+ " ");
+
+        }
+        System.out.println(") Цена букета: " +flower*price);
+    }
 }
 
 class TestFlower {
     public static void main(String[] args) {
         Flower bouquet1 = new Flower();
+        bouquet1.makeBouquet(15,13.0, "Роза");
+        bouquet1.makeBouquet(9,9.0, "Тюльпан");
+        bouquet1.makeBouquet(5,32.0, "Лилия");
+
+
     }
 }
