597f59cb0aa6f4bb38c2e7749f0fb335fb05d7f9
cepera <skuridin@gmail.com>
2020-02-03 22:52:29 +0300

    HomeWork 8 Goods.java

--- Life/src/com/company/Lesson8/Goods.java
+++ Life/src/com/company/Lesson8/Goods.java
@@ -4,21 +4,37 @@ public class Goods {
     String name;
     double price;
     int rating;
+
+    public Goods(String name, double price, int rating){
+        this.name = name;
+        this.price= price;
+        this.rating = rating;
+    }
 }
 
 class Category {
     String name;
     Goods [] goods;
+   // public Category(String name, String[]...Goods){
+
+   // }
 }
 
+class Basket {
+    public void addToBasket(Goods good){
+
+    }
+}
 class TestGoods{
     public static void main(String[] args) {
         Category ct1 = new Category();
         Category ct2 = new Category();
         Category ct3 = new Category();
 
-        Goods goods1 = new Goods();
+        Goods goods1 = new Goods("Сумка",20.0,5);
+        Goods goods2 = new Goods("Туфли",210.0,4);
     }
+
 
 }
 
