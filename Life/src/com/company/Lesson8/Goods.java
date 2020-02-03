7f7d7709a71c1cbe3ab86fac5b856063e89f8f11
cepera <skuridin@gmail.com>
2020-02-03 19:22:31 +0300

    Lesson 8 inet mag

---
+++ Life/src/com/company/Lesson8/Goods.java
@@ -0,0 +1,24 @@
+package com.company.Lesson8;
+
+public class Goods {
+    String name;
+    double price;
+    int rating;
+}
+
+class Category {
+    String name;
+    Goods [] goods;
+}
+
+class TestGoods{
+    public static void main(String[] args) {
+        Category ct1 = new Category();
+        Category ct2 = new Category();
+        Category ct3 = new Category();
+
+        Goods goods1 = new Goods();
+    }
+
+}
+
