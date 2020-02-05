6a7266e2e481846d25a0cfd0f169ceb28203f990
cepera <skuridin@gmail.com>
2020-02-05 18:01:13 +0300

    HomeWork строки

---
+++ jet/src/HomeWork3/Home7.java
@@ -0,0 +1,14 @@
+package HomeWork3;
+// добавлние одной стрки в конец другой
+public class Home7 {
+    String s1 ="упражнения PHP";
+    String s2 = "упражнения Python";
+
+    public static void main(String[] args) {
+        String s1 ="упражнения PHP и ";
+        String s2 = "упражнения Python";
+        System.out.println(s1.concat(s2));
+
+
+    }
+}
