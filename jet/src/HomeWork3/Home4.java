a0e10b8f42990d58f2c82a6d58e743d2037605c0
cepera <skuridin@gmail.com>
2020-02-05 18:13:37 +0300

    HomeWork строки

---
+++ jet/src/HomeWork3/Home4.java
@@ -0,0 +1,10 @@
+package HomeWork3;
+
+public class Home4 {
+    public static void main(String[] args) {
+        String str1 = "w3resource.com";
+        String sub = str1.substring(2,6);
+        System.out.println(sub.length());
+
+    }
+}
