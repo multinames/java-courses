e08f74f638b50197bead2166bc6e492a08296d79
cepera <skuridin@gmail.com>
2020-02-05 18:01:55 +0300

    HomeWork строки

--- jet/src/HomeWork3/Home1.java
+++ jet/src/HomeWork3/Home1.java
@@ -1,5 +1,5 @@
 package HomeWork3;
-//
+// 1.	Напишите программу на Java, чтобы получить символ по указанному индексу в строке
 public class Home1 {
     public static void main(String[] args) {
         String str1 = "упражнения Java!";
--- jet/src/HomeWork3/Home2.java
+++ jet/src/HomeWork3/Home2.java
@@ -1,5 +1,5 @@
 package HomeWork3;
-
+// Напишите программу на Java, чтобы получить символ (кодовую точку Unicode) по указанному индексу в строке.
 public class Home2 {
     public static void main(String[] args) {
         String str1 = "w3resource.com";
