623fad0ba16be7339b3759c18e1ccaa56526dee4
cepera <skuridin@gmail.com>
2020-02-08 12:38:23 +0300

    Lesson10 soap б first day

--- jet/jetweb/src/main/java/app/controller/GoodController.java
+++ jet/jetweb/src/main/java/app/controller/GoodController.java
@@ -1,42 +1,50 @@
 package app.controller;
 
 import app.entity.Good;
+import app.service.GoodService;
 import app.service.GoodServiceInMemory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class GoodController {
+
+    @Autowired
+    @Qualifier("goodServiceInMemory")
+    private GoodService service;
+
     @GetMapping("/get")
     public List<Good> getGoods(){
-        GoodServiceInMemory gs = new GoodServiceInMemory();
-        return gs.get();
+
+        return service.get();
     }
 
     @PostMapping("/add")
     public boolean add (@RequestBody Good good) {
-        GoodServiceInMemory gs = new GoodServiceInMemory();
-        gs.insert(good);
+
+        service.insert(good);
         return true;
     }
 
     @DeleteMapping("/del")
     public boolean delete(@RequestBody int id) {
-        GoodServiceInMemory gs = new GoodServiceInMemory();
-        gs.delete(id);
+
+        service.delete(id);
         return true;
     }
 
     @GetMapping("/get/{id}")
     public Good get(@PathVariable("id")int id) {
-        GoodServiceInMemory gs = new GoodServiceInMemory();
-        return gs.get(id);
+
+        return service.get(id);
     }
     @PostMapping("/update")
     public boolean update(@RequestBody Good good) {
-        GoodServiceInMemory gs = new GoodServiceInMemory();
-        gs.update(good);
+
+        service.update(good);
         return true;
     }
 
