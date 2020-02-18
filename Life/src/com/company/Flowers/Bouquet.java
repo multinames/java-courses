9e5c5fd937c50a3c5c84a4db20c11fd1a5313f0d
cepera <skuridin@gmail.com>
2020-02-18 23:12:11 +0300

    Flower method fix

--- Life/src/com/company/Flowers/Bouquet.java
+++ Life/src/com/company/Flowers/Bouquet.java
@@ -1,25 +1,44 @@
 package com.company.Flowers;
 
+import java.util.ArrayList;
+
 public class Bouquet {
 
     public void createBouquet() {
 
     }
 
-    }
+}
 
 class CreateBouquet {
     public static void main(String[] args) {
-        Flower rose = new Rose();
-        Flower tulip = new Tulip();
-        Flower lily = new Lily();
-        Flower carnation = new Carnation();
+        Flower rose = new Rose(85.5,"Одна роза");
+        Flower tulip = new Tulip(55.3,"Один тюльпан");
+        Flower lily = new Lily(102.5,"Одна лилия");
+        Flower carnation = new Carnation(69.0,"Одна гвоздика");
 
-        rose.setPrice(80.5);
+       /* rose.setPrice(80.5);
         tulip.setPrice(30.5);
         lily.setPrice(120.4);
-        carnation.setPrice(101.2);
+        carnation.setPrice(101.2);*/
 
-        Bouquet bouquet1 = new Bouquet();
+
+
+
+        ArrayList<Flower> buk1 = new ArrayList<>();
+        buk1.add(rose);
+        buk1.add(rose);
+        buk1.add(rose);
+        buk1.add(lily);
+        buk1.add(lily);
+
+        System.out.println(lily);
+
+        for (Flower bu : buk1 ) {
+
+            System.out.println(bu);
+
+        }
+
     }
 }
--- Life/src/com/company/Flowers/Flower.java
+++ Life/src/com/company/Flowers/Flower.java
@@ -1,25 +1,56 @@
 package com.company.Flowers;
 
 public class Flower {
+    private String name;
     private double price;
 
-    public void setPrice(double price) {
+    /*public void setPrice(double price) {
         this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }*/
+
+    public Flower (double price, String name){
+        this.price = price;
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Flower{" +
+                "price=" + price +
+                 " name=" + name +
+                '}';
     }
 }
 
 class Rose extends Flower {
 
+    public Rose(double price, String name) {
+        super(price,name);
+    }
 }
 
 class Tulip extends Flower {
 
+    public Tulip(double price,String name) {
+        super(price,name);
+    }
 }
 
 class Carnation extends Flower {
 
+    public Carnation(double price,String name) {
+        super(price,name);
+    }
 }
 
 class Lily extends Flower {
 
+    public Lily(double price, String name) {
+        super(price, name);
+    }
 }
