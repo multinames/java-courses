fe3c9b0242d242b7fb820187177da4c3ef982283
cepera <skuridin@gmail.com>
2020-02-25 23:29:49 +0300

    JDBC Lesson

--- jet/FlowerDB/src/main/java/app/Main.java
+++ jet/FlowerDB/src/main/java/app/Main.java
@@ -1,18 +1,50 @@
 package app;
 
-import com.mysql.fabric.jdbc.FabricMySQLDriver;
+//import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.SQLException;
+
+import java.sql.*;
 
 public class Main {
+    private static final String URL= "jdbc:mysql://localhost:3306/javat";
+    private final static String URLFIXED =
+            "jdbc:mysql://localhost:3306/javat?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+                    "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String USERNAME = "javat";
+    private static final String PASSWORD = "java123";
+
+
     public static void main(String[] args) {
-        Connection connection;
+        Connection connection = null;
         try {
-            Driver driver = new FabricMySQLDriver();
+            connection=
+            DriverManager.getConnection(URLFIXED,USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+
+             // statement.execute("INSERT INTO animal(animal_name, animal_des) VALUES('cat1','good_cat1')");
+             //statement.execute("select * from animal");
+           // int res = statement.executeUpdate("UPDATE animal SET animal_name='NewName' WHERE id=1");
+            //ResultSet res = statement.executeQuery("SELECT * from animal");
+
+
+            // System.out.println(res);
+
+            statement.addBatch("INSERT INTO animal(animal_name, animal_des) VALUES('cat11','good_cat12')");
+            statement.addBatch("INSERT INTO animal(animal_name, animal_des) VALUES('cat2','good_cat22')");
+            statement.addBatch("INSERT INTO animal(animal_name, animal_des) VALUES('cat3','good_cat23 ')");
+            statement.executeBatch();
+
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+            }
+            connection.close();
+
+            if (connection.isClosed()) {
+                System.out.println("Соединение с БД закрыто");
+            }
         } catch (SQLException e) {
-            System.err.println("Не удалось загрузить класс драйвера");
+            //System.err.println("Что то пошло не так....");
+            e.printStackTrace();
         }
 
     }
