20d84d58a8c75463aa2efa5d965405279856735c
cepera <skuridin@gmail.com>
2020-08-21 14:45:01 +0300

    JavaRush + SecondGuest

--- Life/src/com/company/JavaRush/Jarvis.java
+++ Life/src/com/company/JavaRush/Jarvis.java
@@ -1,12 +1,17 @@
 package com.company.JavaRush;
 
 public class Jarvis {
-    public void sayHi(String name) {
-        System.out.println("Добрый вечер, " + name + ", как ваши дела?");
+    public void sayHi(String firstGuest) {
+        System.out.println("Добрый вечер, " + firstGuest + ", как ваши дела?");
+    }
+
+    public void sayHi(String firstGuest, String secondGuest) {
+        System.out.println("Добрый вечер, " + firstGuest + ", " + secondGuest + ", как ваши дела?");
     }
 
     public static void main(String[] args) {
         Jarvis jarvis = new Jarvis();
         jarvis.sayHi("Тони Старк");
+        jarvis.sayHi("Тони Старк", "Капитан Америка");
     }
 }
