d6756d0ffef9567e39ecf66a5ec73241db0a3639
cepera <skuridin@gmail.com>
2020-08-21 14:48:27 +0300

    JavaRush прегрузка методов...more...

--- Life/src/com/company/JavaRush/Jarvis.java
+++ Life/src/com/company/JavaRush/Jarvis.java
@@ -2,9 +2,9 @@ package com.company.JavaRush;
 
 public class Jarvis {
 
-    public void sayHi(String...names) {
+    public void sayHi(String... names) {
 
-        for (String name: names) {
+        for (String name : names) {
             System.out.println("Добрый вечер, " + name + ", как ваши дела?");
         }
     }
