bb9c088df035cc95ee0245eaff74e8a16744b05e
cepera <skuridin@gmail.com>
2020-08-21 14:47:44 +0300

    JavaRush прегрузка методов....

--- Life/src/com/company/JavaRush/Jarvis.java
+++ Life/src/com/company/JavaRush/Jarvis.java
@@ -1,17 +1,16 @@
 package com.company.JavaRush;
 
 public class Jarvis {
-    public void sayHi(String firstGuest) {
-        System.out.println("Добрый вечер, " + firstGuest + ", как ваши дела?");
-    }
 
-    public void sayHi(String firstGuest, String secondGuest) {
-        System.out.println("Добрый вечер, " + firstGuest + ", " + secondGuest + ", как ваши дела?");
+    public void sayHi(String...names) {
+
+        for (String name: names) {
+            System.out.println("Добрый вечер, " + name + ", как ваши дела?");
+        }
     }
 
     public static void main(String[] args) {
         Jarvis jarvis = new Jarvis();
-        jarvis.sayHi("Тони Старк");
-        jarvis.sayHi("Тони Старк", "Капитан Америка");
+        jarvis.sayHi("Тони Старк", "Капитан Америка", "Черная Вдова", "Халк");
     }
 }
