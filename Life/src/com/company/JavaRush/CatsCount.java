6eda1a25b19db69b272e5cb84757c3975d8c0767
cepera <skuridin@gmail.com>
2020-08-25 18:15:12 +0300

    JavaRush 4 level

---
+++ Life/src/com/company/JavaRush/CatsCount.java
@@ -0,0 +1,21 @@
+package com.company.JavaRush;
+
+public class CatsCount {
+
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat();
+        //напишите тут ваш код
+        Cat.count=++Cat.count;
+
+        Cat cat2 = new Cat();
+        Cat.count=++Cat.count;
+
+        System.out.println("The cat count is " + Cat.count);
+    }
+
+
+    public static class Cat {
+        public static int count = 0;
+    }
+}
--- Life/src/com/company/JavaRush/InputKey.java
+++ Life/src/com/company/JavaRush/InputKey.java
@@ -3,12 +3,17 @@ package com.company.JavaRush;
 import java.util.Scanner;
 
 public class InputKey {
+
     public static void main(String[] args) {
+        System.out.println(toSum(145));
+        }
 
-    System.out.println(getToMet(243));
-    }
-
-    public static int getToMet(int sm){
-        return sm/100;
+    public static int toSum (int number) {
+        int sum = 0;
+        for (;number>0;number /=10) {
+            int number_create = number % 10;
+            sum = sum + number_create;
+        }
+        return sum;
     }
 }
