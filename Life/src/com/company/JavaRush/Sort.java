76b810656bb9e8e915fe549b7b250c0c7bcd1120
cepera <skuridin@gmail.com>
2020-08-31 13:25:45 +0300

    JavaRush 4 level boolean

--- Life/src/com/company/JavaRush/Sort.java
+++ Life/src/com/company/JavaRush/Sort.java
@@ -12,19 +12,37 @@ public class Sort {
         String a= reader.readLine();
         String b= reader.readLine();
         String c= reader.readLine();
+
         int n1 = Integer.parseInt(a);
         int n2 = Integer.parseInt(b);
         int n3 = Integer.parseInt(c);
 
+        int x =0, y=0;
 
-        int x;
-        System.out.println("Числа в переменных a, b и c : " + a  + " " + b + " " + c);
-        if (n1 < n2) {
-            x = n1; n1 = n2; n2 = x;
-        }
-        if (n2 < n3){
-            x = n2; n2 = n3; n3 = x;
-        }
-        System.out.println("Возрастающая последовательность: " + a  + " " + b + " " + c);
+        if (plus(n1)==true & n1!=0)
+            x=x+1;
+        else if(plus(n1)==false & n1!=0)
+            y=y+1;
+
+        if (plus(n2)==true & n2!=0)
+            x=x+1;
+        else if(plus(n2)==false & n2!=0)
+            y=y+1;
+
+        if (plus(n3)==true & n3!=0)
+            x=x+1;
+        else if(plus(n3)==false & n3!=0)
+            y=y+1;
+
+        System.out.println("количество положительных чисел: "+x);
+        System.out.println("количество отрицательных чисел: "+y);
+    }
+
+    public static boolean plus(int a) {
+        boolean b;
+        if (a > 0) b = true;
+        else b=false;
+
+        return b;
     }
 }
