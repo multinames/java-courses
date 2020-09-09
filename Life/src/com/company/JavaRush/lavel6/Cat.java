6df4a9d90bdfbd99660c2b8271048e1cf97fbe05
cepera <skuridin@gmail.com>
2020-09-09 16:07:09 +0300

    JavaRush 6 level static vars

--- Life/src/com/company/JavaRush/lavel6/Cat.java
+++ Life/src/com/company/JavaRush/lavel6/Cat.java
@@ -2,18 +2,22 @@ package com.company.JavaRush.lavel6;
 
 class Cat {
 
-    public static int catCount;
+  private static int catCount =0;
 
-    public Cat() {
-        catCount++;
+  public Cat() {
+      catCount++;
+  }
+
+
+    public static int getCatCount() {
+        return catCount;
+    }
+
+    public static void setCatCount(int catCount) {
+        Cat.catCount=catCount;
     }
     public static void main(String[] args) {
 
     }
 
-
-        protected void finalize() throws Throwable
-        {
-          catCount--;
-        }
 }
